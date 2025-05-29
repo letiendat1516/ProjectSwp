396d090d6801bfa5adc64ea6f1194ddad863c36b
Nguyen Xuan Giap <emiliahikaric@gmail.com>
2025-05-29 21:17:25 +0700

    CRUD Supplier

--- src/java/model/Supplier.java
+++ src/java/model/Supplier.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -115,71 +115,3 @@ public class Supplier {
     }
     
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model;
-
-/**
- *
- * @author Dell
- */
-public class Supplier {
-
-    private int id;
-    private String name;
-    private String address;
-    private String phone;
-    private String email;
-
-    public Supplier(int id, String name, String address, String phone, String email) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
->>>>>>> 9fea14487c16ddecd59badef499efd316ef4a8e2
